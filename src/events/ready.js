bedb18490f3161b8f59312ba7e22fcd2eb344bf5
DewaOnlay <afiuli_alislam@yahoo.com>
2023-02-15 03:29:01 +0700

    first commit

---
+++ src/events/ready.js
@@ -0,0 +1,41 @@
+const { version } = require(`discord.js`)
+const urlsConfig = require(`${process.cwd()}/src/database/UrlsConfig`),
+  AsciiTable = require('ascii-table')
+var table = new AsciiTable()
+table.setBorder('│', '─', "✥", "✥");
+table.setTitle(`Bot is online!`)
+
+module.exports = {
+  async execute(client) {
+
+
+    client.on("ready", () => {
+
+      client.user.setPresence({
+        activities: [{
+          name: `2 Projects`,
+          type: 3
+        }],
+        status: 'idle'
+      })
+
+
+      setTimeout(() => {
+        client.logger((`Logged in as ${client.user.tag}!`).cyan.bold)
+      }, 2000)
+      // Rows
+      table
+        .addRow(`Bot`, client.user.tag)
+        .addRow(`Guild(s)`, `${client.guilds.cache.size} Server(s)`)
+        .addRow(`Member(s)`, `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Members`)
+        .addRow(`Commands`, `${client.slashCommands.size} (Slash)`)
+        .addRow(`Discord.js`, `${version}`)
+        .addRow(`Node.js`, `${process.version}`)
+        .addRow(`Memory`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`)
+        .addRow(`Projects`, `2 Projects`)
+      setTimeout(() => { console.log(table.toString()) }, 3000)
+    })
+  }
+}
+
+
